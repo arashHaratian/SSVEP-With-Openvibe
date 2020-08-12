ba3862fd67c982431dc3cd3faaf3ff7126f54b5f
ArashHaratian <arashharatian@hotmail.com>
2020-08-12 17:55:14 +0430

    adding FBCSP

--- Testing Scenarios/Matlab/carpros.m
+++ Testing Scenarios/Matlab/carpros.m
@@ -4,10 +4,10 @@ for i = 1:OV_getNbPendingInputChunk(box_in,1)
     
     [box_in, start_time, end_time, matrix_data] = OV_popInputBuffer(box_in,1);
     box_in.outputs{1}.header = box_in.inputs{1}.header;
-    mu = mean(matrix_data, 2);
-    for j=1:size(matrix_data, 2)
-        xi = matrix_data(:, j);
-        matrix_data(:, j) = xi-mu;
+    mu = mean(matrix_data, 1);
+    for j=1:size(matrix_data, 1)
+        xi = matrix_data(j, :);
+        matrix_data(j, :) = xi-mu;
     end
 
 box_in = OV_addOutputBuffer(box_in,1,start_time,end_time,matrix_data);
